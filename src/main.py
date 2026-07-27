e701d5b74e7705cfb6d15ad0fb249e005a002f94
AmiuG <andrewghang@gmail.com>
2026-07-26 21:43:31 -0400

    basic app variables added

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+# SIMPLE DEMO AT THIS POINT
+
 from cmu_graphics import *
 import models
 import stats
@@ -10,8 +12,14 @@ graphRight = 600
 graphTop = 80
 graphBottom = 600
 
-dataPanelX = 10
-resultsPanelX = 600
+xMin, xMax = 0, 10
+yMin, yMax = 0, 10
 
 curveColors = ['blue', 'green', 'orange', 'purple', 'red', 'brown']
+
+def onAppStart(app):
+    app.width = windowWidth
+    app.height = windowHeight
+    app.points = []
+    app.results = []
 
