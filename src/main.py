1e94cc881d416b2caed8c5ac4b3b5b2df89d93ff
AmiuG <andrewghang@gmail.com>
2026-07-26 21:33:39 -0400

    hard code constants

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-# from cmu_graphics import *
+from cmu_graphics import *
+import models
+import stats
 
+windowWidth = 940
+windowHeight = 640
 
+graphLeft = 200
+graphRight = 600
+graphTop = 80
+graphBottom = 600
 
+dataPanelX = 10
+resultsPanelX = 600
 
+curveColors = ['blue', 'green', 'orange', 'purple', 'red', 'brown']
 
-
-
-
-# cmu_graphics.run() #initializes cmu graphics
