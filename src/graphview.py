c704232f7e8163e3e2ca5c12588a42314b8a4592
AmiuG <andrewghang@gmail.com>
2026-07-28 17:12:49 -0400

    added functions that convert coordinates from data to pixel and vice versa

--- src/graphview.py
+++ src/graphview.py
@@ -7,9 +7,32 @@ class GraphView:
     def __init__(self, left, top, width, height):
         self.left, self.top = left, top
         self.width, self.height = width, height
-        self.right, self.bottom - left + width, top + height
+        self.right, self.bottom = left + width, top + height
 
         self.xMin, self.xMax = 0, 10
         self.yMin, self.yMin = 0, 10
 
+    def toScreenX(self, x):
+        ratio = (x - self.xMin) / (self.xMax - self.xMin)
+        return self.left + ratio * self.height
+
+    def toScreenY(self, y):
+        ratio = (y - self.yMin) / (self.yMax - self.yMin)
+        return self.bottom - ratio * self.height
+
+    def dataToScreen(self, x, y):
+        return(self.toScreenX(x), self.toScreenY(y))
+
+    def toDataX(self, pixelX):
+        ratio = (pixelX - self.left) / self.width
+        return self.xMin + ratio * (self.xMax - self.xMin)
+
+    def toDataY(self, pixelY):
+        ratio = (self.bottom - pixelY) / self.height
+
+    def screenToData(self, pixelX, pixelY):
+        return (self.toDataX(pixelX), self.toDataY(pixelY))
+
+    def isInPanel(self, pixelX, pixelY):
+        return (self.left <= pixelX <= self.right) and (self.top <= pixelY <= self.bottom)
     
